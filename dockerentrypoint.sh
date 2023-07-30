c259a8fd30bbfbbe0eea0849a4f32f771533c1c1
Yakov Beder <yakovbwork@gmail.com>
2023-07-30 09:19:01 +0000

    Upload New File

---
+++ dockerentrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "Getting artifactory container ID after it was created"
+
+con_id="docker container ls --all --quiet --filter 'name=artifactory*'"
+
+echo "Let me accept an EULA agreement for you"
+
+docker exec -it ${con_id} curl -XPOST -vu admin:password http://$1:8082/artifactory/ui/jcr/eula/accept
+
+echo "EULA agreement has been accepted, let's continue"
